c42728aeecfc0faa21ab3aa74eb69894adc38068
EtherXion <etel0103@gmail.com>
2024-10-11 00:55:23 +0900

    Lv2 - 9 : nickname 등록

--- src/main/java/org/example/expert/config/JwtSecurityFilter.java
+++ src/main/java/org/example/expert/config/JwtSecurityFilter.java
@@ -41,10 +41,11 @@ public class JwtSecurityFilter extends OncePerRequestFilter {
                 Claims claims = jwtUtil.extractClaims(jwt);
                 Long userId = Long.valueOf(claims.getSubject());
                 String email = claims.get("email", String.class);
+                String nickname = claims.get("nickname", String.class);
                 UserRole userRole = UserRole.of(claims.get("userRole", String.class));
 
                 if (SecurityContextHolder.getContext().getAuthentication() == null) {
-                    AuthUser authUser = new AuthUser(userId, email, userRole);
+                    AuthUser authUser = new AuthUser(userId, email, userRole, nickname);
 
                     JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
                     authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
